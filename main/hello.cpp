1f8777416466966dbdd4ea50fdefa006d2c2d6ea
Nidhi Shekhar <shekhar.nidhi.pes@gmail.com>
2025-03-18 16:37:42 +0530

    Re-doing the changed hellofile because of bore error previously

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,5 +7,6 @@ int main(void) {
   cout << "Hello, Jenkins, I am Nidhi - PES1UG22CS382\n";
   cout << "I have successfully built and run this  \n";
   cout << "Added webhook and now i shall change hello file \n";
+  cout << "my previous bore url for the webhook was wrong so re-doing it \n";
   return 0;
 }
